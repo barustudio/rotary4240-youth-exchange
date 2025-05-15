e3d9224b4d1019217359e2401495d320f59ebcfd
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2025-05-15 12:01:27 -0500

    feat: introduce MetaData interface and update layouts to utilize metadata for improved SEO and content management

---
+++ src/types.d.ts
@@ -0,0 +1,38 @@
+interface MetaDataRobots {
+  index?: boolean;
+  follow?: boolean;
+}
+
+interface MetaDataImage {
+  url: string;
+  width?: number;
+  height?: number;
+}
+
+interface MetaDataOpenGraph {
+  url?: string;
+  siteName?: string;
+  images?: MetaDataImage[];
+  locale?: string;
+  type?: string;
+}
+
+interface MetaDataTwitter {
+  handle?: string;
+  site?: string;
+  cardType?: string;
+}
+
+export interface MetaData {
+  title?: string;
+  description?: string;
+
+  ignoreTitleTemplate?: boolean;
+
+  canonical?: string;
+
+  robots?: MetaDataRobots;
+
+  openGraph?: MetaDataOpenGraph;
+  twitter?: MetaDataTwitter;
+}
